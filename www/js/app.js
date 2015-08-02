e93fb8551177b9061388ac9d7470c5869360b89c
Mikael Kindborg <mikael.kindborg@gmail.com>
2015-08-02 20:12:31 +0200

    New connect method that validates they connect key before connecting; added UI files.

--- www/js/app.js
+++ www/js/app.js
@@ -6,13 +6,17 @@ if (window.hyper && window.hyper.log) { console.log = hyper.log; console.error =
 // Application object.
 var app = {}
 
-// Constants.
+// Production server address.
 //var SERVER_ADDRESS = 'http://evothings.com:8081'
-var SERVER_ADDRESS = 'http://192.168.43.131:8081'
+
+// Use your machine's IP address for testing.
+var SERVER_ADDRESS = 'http://192.168.43.247:8081' // Micke's test address
 
 app.initialize = function()
 {
 	document.addEventListener('deviceready', app.onDeviceReady, false)
+
+	app.hideSpinner()
 
 	// Page navigation.
 	$('#info_button').bind('click', {articleId: 'info'}, app.showArticle)
@@ -27,26 +31,87 @@ app.onDeviceReady = function()
 {
 }
 
-app.connect = function()
+app.onConnectButton = function()
 {
-	// Get contents of url text field.
-	var key = document.getElementById("hyper-key").value
+	app.showMessage('Connecting...')
+	app.showSpinner()
 
-	// Set URL.
-	var url = ''
-	if (0 == key.indexOf('http://'))
+	// Get contents of url text field.
+	var keyOrURL = document.getElementById('hyper-key').value.trim()
+
+	// Is it a key code?
+	if (/^\d{4}$/.test(keyOrURL))
+	{
+		// Check if the code exists and connect to the server if ok.
+		app.connectWithKey(keyOrURL)
+	}
+	else if (0 == keyOrURL.indexOf('http://'))
 	{
 		// This looks like a URL, launch it.
-		url = key
+		window.location.assign(keyOrURL)
 	}
 	else
 	{
-		// Create server URL.
-		url = SERVER_ADDRESS + '/hyper/' + key + '/connect/'
+		// Neither key nor URL. Display message.
+		app.showMessage('Please enter a connect key or URL.')
+		app.hideSpinner()
 	}
+}
 
-	// Open URL.
-	window.location.assign(url)
+app.connectWithKey = function(key)
+{
+	// Check that key exists.
+	var requestURL = SERVER_ADDRESS + '/check-connect-key/' + key
+	var request = $.ajax(
+		{
+			timeout: 5000,
+			url: requestURL,
+		})
+
+	// If key exists, connect to Workbench.
+	request.done(function(data)
+	{
+		app.showMessage('Result: ' + data)
+
+		if ('KEY-OK' == data)
+		{
+			// Connect to server.
+			var serverURL = SERVER_ADDRESS + '/connect/' + key
+			app.showMessage('Connecting to: ' + serverURL)
+			window.location.assign(serverURL)
+		}
+		else if ('KEY-NOT-OK' == data)
+		{
+			app.showMessage('Invalid or expired key, please get a new key and try again.')
+			app.hideSpinner()
+		}
+		else
+		{
+			app.showMessage('Something went wrong. Server did not respond as expected. Please report this error.')
+			app.hideSpinner()
+		}
+	})
+
+	request.fail(function(jqxhr)
+	{
+		app.showMessage('Could not connect. Please check your Internet connection and try again.')
+		app.hideSpinner()
+	})
+}
+
+app.showMessage = function(message)
+{
+	$('#hyper-message').html(message)
+}
+
+app.showSpinner = function()
+{
+	$('#hyper-spinner').show()
+}
+
+app.hideSpinner = function()
+{
+	$('#hyper-spinner').hide()
 }
 
 app.openBrowser = function(url)
