659aff20f579e02af9055d641b7f6981d1ac22d1
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-04-27 22:14:14 +0200

    Update for Cordova 6

--- config/native/ios/AppDelegate.m
+++ config/native/ios/AppDelegate.m
@@ -2,10 +2,10 @@
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
  distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
+ regarding copyright ownership.	 The ASF licenses this file
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
+ with the License.	You may obtain a copy of the License at
 
  http://www.apache.org/licenses/LICENSE-2.0
 
@@ -18,166 +18,63 @@
  */
 
 //
-//  AppDelegate.m
-//  Evothings
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//	AppDelegate.m
 //
 
 #import "AppDelegate.h"
 #import "MainViewController.h"
 
-#import <Cordova/CDVPlugin.h>
-
 @implementation AppDelegate
 
-@synthesize window, viewController;
-
-- (id)init
+- (BOOL) application: (UIApplication*)application
+	didFinishLaunchingWithOptions: (NSDictionary*)launchOptions
 {
-    /** If you need to do any extra app-specific initialization, you can do it here
-     *  -jm
-     **/
-    NSHTTPCookieStorage* cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
-
-    [cookieStorage setCookieAcceptPolicy:NSHTTPCookieAcceptPolicyAlways];
-
-    int cacheSizeMemory = 8 * 1024 * 1024; // 8MB
-    int cacheSizeDisk = 32 * 1024 * 1024; // 32MB
-#if __has_feature(objc_arc)
-        NSURLCache* sharedCache = [[NSURLCache alloc] initWithMemoryCapacity:cacheSizeMemory diskCapacity:cacheSizeDisk diskPath:@"nsurlcache"];
-#else
-        NSURLCache* sharedCache = [[[NSURLCache alloc] initWithMemoryCapacity:cacheSizeMemory diskCapacity:cacheSizeDisk diskPath:@"nsurlcache"] autorelease];
-#endif
-    [NSURLCache setSharedURLCache:sharedCache];
-
-    self = [super init];
-    return self;
+	self.viewController = [[MainViewController alloc] init];
+	return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-#pragma mark UIApplicationDelegate implementation
-
-/**
- * This is main kick off after the app inits, the views and Settings are setup here. (preferred - iOS4 and up)
- */
-- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
+- (BOOL)application: (UIApplication*)app
+		openURL: (NSURL*)url
+		options: (NSDictionary<NSString*,id>*)options
 {
-    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+	if (url && [self.viewController.webView isKindOfClass:[UIWebView class]])
+	{
+		NSLog(@"@@@ url scheme: %@", [url scheme]);
 
-#if __has_feature(objc_arc)
-        self.window = [[UIWindow alloc] initWithFrame:screenBounds];
-#else
-        self.window = [[[UIWindow alloc] initWithFrame:screenBounds] autorelease];
-#endif
-    self.window.autoresizesSubviews = YES;
+		// Use https for scheme "evos".
+		NSString* httpScheme =
+			[[url scheme] isEqualToString: @"evos"] ?
+				@"https" :
+				@"http";
 
-#if __has_feature(objc_arc)
-        self.viewController = [[MainViewController alloc] init];
-#else
-        self.viewController = [[[MainViewController alloc] init] autorelease];
-#endif
+		// Load the given URL into the web view.
+		NSString* targetURL =
+			[NSString
+				stringWithFormat: @"%@:%@",
+				httpScheme,
+				[url resourceSpecifier]];
+		NSURLRequest* request =
+			[NSURLRequest
+				requestWithURL: [NSURL URLWithString: targetURL]
+				cachePolicy: NSURLRequestReloadIgnoringLocalAndRemoteCacheData
+				timeoutInterval: 10];
 
-    // Set your app's start page by setting the <content src='foo.html' /> tag in config.xml.
-    // If necessary, uncomment the line below to override it.
-    // self.viewController.startPage = @"index.html";
+		NSLog(@"@@@ targetURL: %@", targetURL);
 
-    // NOTE: To customize the view's frame size (which defaults to full screen), override
-    // [self.viewController viewWillAppear:] in your view controller.
+		[(UIWebView*)self.viewController.webView loadRequest: request];
 
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
+		// all plugins will get the notification, and their handlers will be called
+		[[NSNotificationCenter defaultCenter] postNotification:
+			[NSNotification
+				notificationWithName: CDVPluginHandleOpenURLNotification
+				object: url]];
 
-    return YES;
-}
-
-// Original Cordova code commented out.
-
-/*
-// this happens while we are running ( in the background, or from within our own app )
-// only valid if Evothings-Info.plist specifies a protocol to handle
-- (BOOL)application:(UIApplication*)application openURL:(NSURL*)url sourceApplication:(NSString*)sourceApplication annotation:(id)annotation
-{
-    if (!url) {
-        return NO;
-    }
-
-    // all plugins will get the notification, and their handlers will be called
-    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLNotification object:url]];
-
-    return YES;
-}
-*/
-
-// Updated/patched code that handles Evothings URL:s.
-
-- (BOOL)application:(UIApplication*)application openURL:(NSURL*)url sourceApplication:(NSString*)sourceApplication annotation:(id)annotation
-{
-    if (!url) {
-        return NO;
-    }
-
-    // Load the given URL into the web view.
-    NSString* targetURL = [NSString
-		stringWithFormat: @"http:%@",
-		[url resourceSpecifier]];
-	NSURLRequest* request = [NSURLRequest
-		requestWithURL: [NSURL URLWithString: targetURL]
-		cachePolicy: NSURLRequestReloadIgnoringLocalAndRemoteCacheData
-		timeoutInterval: 10];
-    [self.viewController.webView loadRequest: request];
-
-    // all plugins will get the notification, and their handlers will be called
-    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLNotification object:url]];
-
-    return YES;
-}
-
-// repost all remote and local notification using the default NSNotificationCenter so multiple plugins may respond
-- (void)            application:(UIApplication*)application
-    didReceiveLocalNotification:(UILocalNotification*)notification
-{
-    // re-post ( broadcast )
-    [[NSNotificationCenter defaultCenter] postNotificationName:CDVLocalNotification object:notification];
-}
-
-#ifndef DISABLE_PUSH_NOTIFICATIONS
-
-    - (void)                                 application:(UIApplication*)application
-        didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
-    {
-        // re-post ( broadcast )
-        NSString* token = [[[[deviceToken description]
-            stringByReplacingOccurrencesOfString:@"<" withString:@""]
-            stringByReplacingOccurrencesOfString:@">" withString:@""]
-            stringByReplacingOccurrencesOfString:@" " withString:@""];
-
-        [[NSNotificationCenter defaultCenter] postNotificationName:CDVRemoteNotification object:token];
-    }
-
-    - (void)                                 application:(UIApplication*)application
-        didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
-    {
-        // re-post ( broadcast )
-        [[NSNotificationCenter defaultCenter] postNotificationName:CDVRemoteNotificationError object:error];
-    }
-#endif
-
-#if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
-- (NSUInteger)application:(UIApplication*)application supportedInterfaceOrientationsForWindow:(UIWindow*)window
-#else
-- (UIInterfaceOrientationMask)application:(UIApplication*)application supportedInterfaceOrientationsForWindow:(UIWindow*)window
-#endif
-{
-    // iPhone doesn't support upside down by default, while the iPad does.  Override to allow all orientations always, and let the root view controller decide what's allowed (the supported orientations mask gets intersected).
-    NSUInteger supportedInterfaceOrientations = (1 << UIInterfaceOrientationPortrait) | (1 << UIInterfaceOrientationLandscapeLeft) | (1 << UIInterfaceOrientationLandscapeRight) | (1 << UIInterfaceOrientationPortraitUpsideDown);
-
-    return supportedInterfaceOrientations;
-}
-
-- (void)applicationDidReceiveMemoryWarning:(UIApplication*)application
-{
-    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+		return YES;
+	}
+	else
+	{
+		return NO;
+	}
 }
 
 @end
