9561a820d8913033a453e23ed6e282708104f087
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-10-04 13:03:22 +0200

    Added script for easy rebuild of the BLE plugin while developing.

---
+++ rebuild-ble-plugin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Remove and add BLE plugin, build android.
+#
+cordova plugin rm cordova-plugin-ble
+cordova plugin add ../cordova-ble
+cordova build ios
+cordova build android
+adb install -r ./platforms/android/build/outputs/apk/android-debug.apk
